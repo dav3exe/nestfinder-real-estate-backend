03202ae4ae26c8c83c546cea834e7372a8f4edbb
dav3exe <ogungbemitolulope2004@gmail.com>
2026-04-27 23:42:51 +0100

    changes

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,6 @@ app.use(
     origin: [
       process.env.CLIENT_URL || "http://localhost:5173",
       "https://nestfinder-real-estate.vercel.app/",
-      "http://localhost:5173",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
